5d2a1b10784823e91098cbac6925a32329e487f6
Stephen Nixon <swr410@gmail.com>
2019-01-30 09:15:30 -0500

    add detail to docstring

--- bin/gftools-fix-isfixedpitch.py
+++ bin/gftools-fix-isfixedpitch.py
@@ -1,4 +1,39 @@
-"""Fix a collection of fonts isfixedpitch flag"""
+#!/usr/bin/env python
+
+# Copyright 2013,2016 The Font Bakery Authors.
+# Copyright 2017 The Google Font Tools Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# See AUTHORS.txt for the list of Authors and LICENSE.txt for the License.
+
+"""
+It is important for monospaced fonts to have the 'isFixedPitch' flag set to 1
+in the post table. If it is set to 0, Windows coding programs categorize them
+as proportional fonts, and they will not appear as options font selection 
+preferences.
+\n
+This script checks whether a font or collection of fonts appears to be monospaced 
+by checking for equal widths in the entire Latin lowercase, a to z. If these are 
+equal, 'isFixedPitch' will be set to 1. If a-z are unequal, 'isFixedPitch' will be 
+set to 0.
+
+Usage:
+
+gftools-fix-isfixedpitch --fonts <font> <font> <font>
+
+"""
+
 from fontTools.ttLib import TTFont
 import argparse
 
