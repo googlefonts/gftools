d8f8c1a233351525dd2084c89cdc9efc4a232396
Marc Foley <m.foley.88@gmail.com>
2019-10-08 15:35:47 +0100

    test/run.sh: add gftools unicode-names

--- test/run.sh
+++ test/run.sh
@@ -48,7 +48,7 @@ gftools-rangify.py ./data/arabic_unique-glyphs.nam
 # gftools-sanity-check.py ./data/mock_googlefonts/ofl/abel/ DISABLED does not follow nww spec
 # gftools-test-gf-coverage.py
 # gftools-ttf2cp.py
-# gftools-unicode-names.py
+gftools-unicode-names.py arabic_unique-glyphs.nam
 gftools-update-families.py ./data/Lora-Regular.ttf
 gftools update-version ./data/Lora-Regular.ttf 1.000 2.000
 gftools varfont-info ./data/Lora-Roman-VF.ttf
