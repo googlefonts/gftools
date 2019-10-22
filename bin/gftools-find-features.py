34f05258babd71277ed9117f58981d9440ead260
Marc Foley <m.foley.88@gmail.com>
2019-10-22 14:54:58 +0100

    gftools find-features: fix script and make py3 compatible

--- bin/gftools-find-features.py
+++ bin/gftools-find-features.py
@@ -20,86 +20,52 @@
 from __future__ import print_function
 import contextlib
 import os
-
-from fontTools import ttLib
+import sys
+from fontTools.ttLib import TTFont
 from gftools.util import google_fonts as fonts
 from absl import app
 
 
-def ListFeatures(font, table, name_fn):
+def ListFeatures(font):
   """List features for specified font. Table assumed structured like GPS/GSUB.
 
   Args:
     font: a TTFont.
-    table: name of table.
-    name_fn: function to resolve lookup type => name.
   Returns:
     List of 3-tuples of ('GPOS', tag, name) of the features in the font.
   """
   results = []
-  if table not in font:
-    return results
-
-  tab = font[table].table
-  if not tab or not tab.LookupList or not tab.LookupList.Lookup:
-    return results
-  lookups = tab.LookupList.Lookup
-
-  for feat in tab.FeatureList.FeatureRecord:
-    tag = feat.FeatureTag
-    for idx in feat.Feature.LookupListIndex:
-      lookup_name = name_fn(lookups[idx].LookupType)
-      results.append((table, tag, lookup_name))
-
+  for tbl in ["GPOS", "GSUB"]:
+    if tbl in font.keys():
+      results += [
+        (tbl,
+         f.FeatureTag,
+         "lookups: [{}]".format(", ".join(map(str, f.Feature.LookupListIndex)))
+        ) for f in font[tbl].table.FeatureList.FeatureRecord
+      ]
   return results
 
 
-def FindFonts(path):
-  """Find one weight per family in each font directory in path.
-
-  Args:
-    path: A directory.
-  Returns:
-    A list of full paths to font files in Google-fonts style directories under
-    path. Selects one weight per family.
-  """
-  files = []
-  for font_dir in fonts.FontDirs(path):
-    try:
-      metadata = fonts.Metadata(font_dir)
-    except ValueError:
-      print('Bad METADATA.pb for %s' % font_dir)
-      continue
-
-    try:
-      filename = fonts.RegularWeight(metadata)
-    except OSError:
-      print('No RegularWeight for %s' % font_dir)
-      filename = metadata.fonts[0].filename
-
-    files.append(os.path.join(font_dir, filename))
-
-  return files
-
-
-def main(argv):
-  for path in argv[1:]:
+def main(path):
+  if path.endswith(".ttf"):
     font_files = [path]
-    if os.path.isdir(path):
-      font_files = FindFonts(path)
+  elif os.path.isdir(path):
+    font_files = glob(path + "/*.ttf")
 
-    for font_file in font_files:
-      features = []
-      try:
-        with contextlib.closing(ttLib.TTFont(font_file)) as font:
-          features += ListFeatures(font, 'GSUB', fonts.GsubLookupTypeName)
-          features += ListFeatures(font, 'GPOS', fonts.GposLookupTypeName)
-      except IOError:
-        print('%s does not exist or is not a valid font' % font_file)
-      for (table, tag, lookup_name) in features:
-        print('{:32s} {:4s} {:8s} {:15s}'.format(
-            os.path.basename(font_file), table, str(tag), lookup_name))
+  for font_file in font_files:
+    features = []
+    with TTFont(font_file) as font:
+      features += ListFeatures(font)
+
+    for (table, tag, lookup_name) in features:
+      print('{:32s} {:4s} {:8s} {:15s}'.format(
+      os.path.basename(font_file), table, str(tag), lookup_name))
 
 
 if __name__ == '__main__':
-  app.run(main)
+  if len(sys.argv) != 2:
+    print("Please include either a path to a ttf or a path to a dir "
+          "containing ttfs")
+  else:
+    main(sys.argv[1])
+
