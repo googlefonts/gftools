dc41fd88e95db1e7ca45f605b369b13987a9cb17
Marc Foley <m.foley.88@gmail.com>
2020-04-06 16:19:18 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open('README.md') as f:
 
 setup(
     name="gftools",
-    version='0.3.6',
+    version='0.3.7',
     url='https://github.com/googlefonts/tools/',
     description='Google Fonts Tools is a set of command-line tools'
                 ' for testing font projects',
