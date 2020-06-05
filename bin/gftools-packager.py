f7583200639035812d37df83aaa734c5c3101d05
Lasse Fister <commander@graphicore.de>
2020-06-05 17:12:17 +0200

    [packager] WIP add cli arguments for target management.

--- bin/gftools-packager.py
+++ bin/gftools-packager.py
@@ -20,6 +20,32 @@ parser.add_argument(
             action='store_true',
             help='Load upstream.yaml from a file, use the [name] argument as path.')
 parser.add_argument(
+            'target',
+            type=str,
+            help='The target of the package. By default a path to a directory. '
+                  'See -f/--force to allow changing none-empty directories. '
+                  'See -g/--gf-git to use it as a git repository.')
+parser.add_argument(
+            '-g','--gf-git',
+            dest='is_gf_git',
+            action='store_true',
+            help='Try to use target as a git repository clone of GitHub google/fonts and '
+                 'create or override a branch from upstream master using a generated '
+                 'default branch name or a branch name specified with -b/--branch')
+parser.add_argument(
+            '-b', '--branch',
+            type=str,
+            default=None,
+            help='When -g/--gf-git is used, set the local target branch name instead '
+                 'of using the generated branch name, like: "gftools-packager-{family-name}".')
+parser.add_argument(
+            '-f','--force',
+            action='store_true',
+            help='This allows the program to manipulate/change/delete data '
+                 'in [target]. Without this flag only adding new items '
+                 '(depends: files, directories or branches, trees, blobs) '
+                 'is allowed.')
+parser.add_argument(
             '-y',
             '--no-confirm',
             dest='yes',
@@ -34,7 +60,7 @@ parser.add_argument(
             action='store_true',
             help='Don\'t print user interaction dialogues when --no-confirm is used.')
 parser.add_argument(
-            '--no_whitelist',
+            '--no-whitelist',
             action='store_true',
             help='Don\'t use the whitelist of allowed files to copy from '
                  'TARGET in upstream_conf.files. This is meant to enable '
