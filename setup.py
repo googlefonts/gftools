2ab2d3a648548621078949456f8125e1213e3674
Marc Foley <m.foley.88@gmail.com>
2020-11-18 11:37:28 +0000

    Use use_scm_version instead of manually bumping version number

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open('README.md') as f:
 
 setup(
     name="gftools",
-    version='0.5.1',
+    use_scm_version={"write_to": "Lib/gftools/_version.py"},
     url='https://github.com/googlefonts/tools/',
     description='Google Fonts Tools is a set of command-line tools'
                 ' for testing font projects',
