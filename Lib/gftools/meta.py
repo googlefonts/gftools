d71b3d62d119ece0cc6dcd18fb492fa1594a358e
Simon Cozens <simon@simon-cozens.org>
2021-08-19 11:03:28 +0100

    Code to create meta tables with language tag information

---
+++ Lib/gftools/meta.py
@@ -0,0 +1,75 @@
+"""Generate meta table for a font
+
+This module exports the function "gen_meta_table" which can be used to
+add a meta table designating supported and/or design scripts/languages.
+"""
+from fontTools.ttLib import TTFont, newTable
+import os
+import logging
+import yaml
+import re
+from langcodes import Language
+from langcodes.tag_parser import LanguageTagError
+from langcodes.data_dicts import VALIDITY
+
+__all__ = ["gen_meta_table"]
+
+
+log = logging.getLogger(__name__)
+
+
+def gen_meta_table(ttFont, config):
+    """Generates a meta table from a configuration dictionary.
+
+    Args:
+        ttFont: a TTFont instance
+        config: a dictionary containing ``slng``/``dlng`` keys
+    """
+    assert isinstance(config, dict)
+    if "meta" in ttFont:
+        meta = ttFont["meta"]
+        log.warning("meta table already found; merging configuration values!")
+    else:
+        ttFont["meta"] = meta = newTable("meta")
+    for key in ["slng", "dlng"]:
+        if key not in config:
+            continue
+        values = config[key]
+        if isinstance(values, str):
+            values = re.split(r",\s*", values)
+        if key in meta.data:
+            values.extend(re.split(r",\s*", meta.data[key]))
+        value = _language_list_to_string(values)
+        meta.data[key] = value
+
+
+def _language_list_to_string(values):
+    return ", ".join(
+        sorted([value for value in set(values) if _validate_scriptlangtag(value)])
+    )
+
+
+def _validate_scriptlangtag(value):
+    try:
+        code = Language.get(value)
+    except LanguageTagError:
+        log.warning(f"Skipping unparsable script/lang tag '{value}'")
+        return False
+    if not code.script:
+        log.warning(
+            f"meta table script/lang tags must have a script component, '{value}' does not"
+        )
+        return False
+    if code.is_valid():
+        return True
+    if code.language and not VALIDITY.match(code.language):
+        log.warning(f"Invalid language code '{code.language}' in '{value}'")
+    if not VALIDITY.match(code.script):
+        log.warning(f"Invalid script code '{code.script}' in '{value}'")
+    if code.territory and not VALIDITY.match(code.territory):
+        log.warning(f"Invalid territory code '{code.territory}' in '{value}'")
+    if code.variants:
+        for variant in code.variants:
+            if not VALIDITY.match(variant):
+                log.warning(f"Invalid variant code '{variant}' in '{value}'")
+    return False
---
+++ Lib/gftools/tests/test_meta.py
@@ -0,0 +1,38 @@
+import pytest
+import os
+from glob import glob
+from gftools.meta import gen_meta_table
+from fontTools.ttLib import TTFont
+
+
+TEST_DATA = os.path.join("data", "test")
+
+
+def test_meta_where_none_present():
+    a_font = TTFont(os.path.join(TEST_DATA, "Inconsolata[wdth,wght].ttf"))
+    assert "meta" not in a_font
+    gen_meta_table(a_font, {
+        "slng": ["en-Latn", "tur-Hang-IT", "tur"],
+        "dlng": ["en-Latn-AZ"]
+    })
+    assert a_font["meta"].data == {
+        "slng": "en-Latn, tur-Hang-IT",
+        "dlng": "en-Latn-AZ"
+    }
+
+def test_meta_where_one_present():
+    a_font = TTFont(os.path.join(TEST_DATA, "Inconsolata[wdth,wght].ttf"))
+    assert "meta" not in a_font
+    gen_meta_table(a_font, {
+        "slng": ["en-Latn", "tur-Hang-IT", "tur"],
+        "dlng": ["en-Latn-AZ"]
+    })
+    assert "meta" in a_font
+    gen_meta_table(a_font, {
+        "slng": ["es-Arab-RS-spanglis"],
+    })
+
+    assert a_font["meta"].data == {
+        "slng": "en-Latn, es-Arab-RS-spanglis, tur-Hang-IT",
+        "dlng": "en-Latn-AZ"
+    }
