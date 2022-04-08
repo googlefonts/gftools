a457b36b28483a719c38ccae4ce5bf6e70a94596
Simon Cozens <simon@simon-cozens.org>
2022-04-08 11:47:42 +0100

    pass_environment requires jinja >=3

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ setup(
         'brotli',
         'browserstack-local==1.2.2',
         'pybrowserstack-screenshots==0.1',
-        'jinja2',
+        'jinja2>=3.0.0',
         'hyperglot',
         'fontFeatures',
         'vharfbuzz',
