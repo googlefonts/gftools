6c0bfebe4c7ba79854fd839d588d91116f9e3817
Marc Foley <m.foley.88@gmail.com>
2022-06-07 11:42:10 +0100

    cleanup

--- bin/gftools-ufo-mm-fix.py
+++ bin/gftools-ufo-mm-fix.py
@@ -1,39 +1,61 @@
+#!/usr/bin/env python3
 """
-Primitive prepolator!
+Primitive ufo prepolator!
+
+Fix ufo and designspace glyph incompatibilities. It can
+currently fix:
+- starting points
+- contour order
+- component order
+
+Users will need to manually fix paths which have different node counts.
 
 Warning mega destructive! TODO work on duplicates instead of overwriting.
 Always use version control!
 
-This may take a while to run so grab dinner
+This may take a while for families which contain many masters.
+
+
+Usage:
+
+# fix ufos
+gftools ufo-fix-mm font1.ufo font2.ufo
+
+# fix designspaces
+gftools ufo-fix-mm family.designspace
 """
+from fontTools.designspaceLib import DesignSpaceDocument
 from fontTools.varLib.interpolatable import test
-from fontmake.compatibility import CompatibilityChecker
 from ufoLib2 import Font
-import sys
 import os
 from itertools import permutations
+import argparse
 
 
 def _fix_starting_points(glyphsets, base_font, font, glyph, names, idx):
-    aa = base_font[glyph]
-    gg = font[glyph]
+    base_glyph = base_font[glyph]
+    font_glyph = font[glyph]
     font.save()
-    res = test([glyphsets[0], font.reader.getGlyphSet()], glyphs=[glyph], names=names)
-    res2 = point_compat(base_font, font, glyph)
-    if len(res) == 0 and res2:
-        print(f"Fixed {glyph}")
+    test_order = test(
+        [glyphsets[0], font.reader.getGlyphSet()], glyphs=[glyph], names=names
+    )
+    test_compatibility = nodes_compatible(base_font, font, glyph)
+    if len(test_order) == 0 and test_compatibility:
+        print(f"{glyph}: Fixed starting points")
         return True
-    if idx == len(gg):
+    if idx == len(font_glyph):
         return False
-    
-    for _ in range(len(gg[idx])):
-        gg[idx].append(gg[idx].pop(0))
-        gg_types = [n.type for n in gg[idx]]
-        aa_types = [n.type for n in aa[idx]]
-        if gg_types != aa_types:
+
+    for _ in range(len(font_glyph[idx])):
+        # incrememt starting point by next node
+        font_glyph[idx].append(font_glyph[idx].pop(0))
+        # check that both paths have the same node types
+        font_nodes = [n.type for n in font_glyph[idx]]
+        base_nodes = [n.type for n in base_glyph[idx]]
+        if font_nodes != base_nodes:
             continue
-        
-        if _fix_starting_points(glyphsets, base_font, font, glyph, names, idx+1):
+
+        if _fix_starting_points(glyphsets, base_font, font, glyph, names, idx + 1):
             return True
     return False
 
@@ -41,28 +63,29 @@ def _fix_starting_points(glyphsets, base_font, font, glyph, names, idx):
 def fix_starting_points(glyphsets, base_font, font, glyph, names):
     fixed = _fix_starting_points(glyphsets, base_font, font, glyph, names, 0)
     if not fixed:
-        raise ValueError(f"{glyph}: shit Curves")
+        print(f"{glyph}: Failed fixing starting points")
+        return False
     return True
 
 
-def point_compat(base_font, font, glyph):
-    base_points = []
-    font_points = []
+def nodes_compatible(base_font, font, glyph):
+    base_font_nodes = []
+    font_nodes = []
 
     for cont in base_font[glyph]:
         for pt in cont:
-            base_points.append(pt.type)
-    
+            base_font_nodes.append(pt.type)
+
     for cont in font[glyph]:
         for pt in cont:
-            font_points.append(pt.type)
-    return base_points == font_points
+            font_nodes.append(pt.type)
+    return base_font_nodes == font_nodes
 
 
-def comp_compat(base_font, font, glyph):
-    base_comps = [c.baseGlyph for c in base_font[glyph].components]
-    font_comps = [c.baseGlyph for c in font[glyph].components]
-    return base_comps == font_comps
+def components_compatible(base_font, font, glyph):
+    base_font_components = [c.baseGlyph for c in base_font[glyph].components]
+    font_components = [c.baseGlyph for c in font[glyph].components]
+    return base_font_components == font_components
 
 
 def fix_contour_order(glyphsets, font, glyph, names):
@@ -73,14 +96,16 @@ def fix_contour_order(glyphsets, font, glyph, names):
         for contour in perm:
             font[glyph].appendContour(contour)
         font.save()
-        res = test([glyphsets[0], font.reader.getGlyphSet()], glyphs=[glyph], names=names)
+        res = test(
+            [glyphsets[0], font.reader.getGlyphSet()], glyphs=[glyph], names=names
+        )
         if len(res) == 0:
-            print(f"{glyph}: fixed")
+            print(f"{glyph}: Fixed contour order")
             return
-    print("failed fixing contour order")
+    print(f"{glyph}: Failed fixing contour order")
 
 
-def fix_composite_order(base_font, font, glyph):
+def fix_component_order(base_font, font, glyph):
     components = font[glyph].components
     perms = permutations(components)
     for perm in perms:
@@ -88,16 +113,37 @@ def fix_composite_order(base_font, font, glyph):
         for comp in perm:
             font[glyph].components.append(comp)
         font.save()
-        res = comp_compat(base_font, font, glyph)
+        res = components_compatible(base_font, font, glyph)
         if res:
-            print(f"{glyph}: fixed")
+            print(f"{glyph}: Fixed component order")
             return
-    print("failed fixing composite order")
-        
+    print(f"{glyph}: Failed fixing component order")
+
 
 def main():
-    names = [os.path.basename(f) for f in sys.argv[1:]]
-    fonts = [Font.open(f) for f in sys.argv[1:]]
+    parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument(
+        "fonts", nargs="+", help="Source files. Either ufos of designspace."
+    )
+    args = parser.parse_args()
+
+    sources = []
+    for src in args.fonts:
+        if src.endswith(".designspace"):
+            ds = DesignSpaceDocument()
+            ds.read(src)
+            for s in ds.sources:
+                if s in sources:
+                    continue
+                sources.append(s.path)
+        else:
+            sources.append(src)
+    
+    if len(sources) == 1:
+        raise ValueError("Single source family! cannot check.")
+
+    names = [os.path.basename(f) for f in sources[1:]]
+    fonts = [Font.open(f) for f in sources[1:]]
     glyphsets = [f.reader.getGlyphSet() for f in fonts]
     base_font = fonts[0]
     fonts = fonts[1:]
@@ -107,23 +153,28 @@ def main():
 
     glyphs = base_font.reader.getGlyphSet().keys()
     for glyph in glyphs:
+        # Check each font against the base font
         for idx, glyphset in enumerate(glyphsets):
             font = fonts[idx]
             res = test([base_glyphset, glyphset], glyphs=[glyph], names=names)
-            points_compat = point_compat(base_font, font, glyph)
-            comps_compat = comp_compat(base_font, font, glyph)
+            points_compat = nodes_compatible(base_font, font, glyph)
+            comps_compat = components_compatible(base_font, font, glyph)
             if all([not res, points_compat, comps_compat]):
                 continue
             if not comps_compat:
-                fix_composite_order(base_font, font, glyph)
-            if any(i['type'] == "contour_order" for i in res[glyph]):
+                fix_component_order(base_font, font, glyph)
+            if any(i["type"] == "contour_order" for i in res[glyph]):
                 fix_contour_order([base_glyphset, glyphset], font, glyph, names)
             if not points_compat:
-                fix_starting_points([base_glyphset, glyphset], base_font, font, glyph, names)
+                fix_starting_points(
+                    [base_glyphset, glyphset], base_font, font, glyph, names
+                )
             if res:
                 for issue in res[glyph]:
-                    if issue['type'] in ["wrong_start_point", "node_incompatibility"]:
-                        fix_starting_points([base_glyphset, glyphset], base_font, font, glyph, names)
+                    if issue["type"] in ["wrong_start_point", "node_incompatibility"]:
+                        fix_starting_points(
+                            [base_glyphset, glyphset], base_font, font, glyph, names
+                        )
                     else:
                         print("Cannot fix")
                         print(issue)
