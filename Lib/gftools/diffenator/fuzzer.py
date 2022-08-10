1d6db16e7cebd06fc7961fcf893692b16f7ccf5e
Marc Foley <m.foley.88@gmail.com>
2022-08-10 10:58:13 +0100

    wip fuzz2

--- Lib/gftools/diffenator/fuzzer.py
+++ Lib/gftools/diffenator/fuzzer.py
@@ -1,4 +1,3 @@
-from fontTools.ttLib import TTFont
 import sys
 import random
 import subprocess
@@ -31,10 +30,58 @@ def fuzz_fonts(old_font, new_font, duration=60):
     return res
 
 
+
+def input_fuzz(font):
+    from gftools.diffenator import DFont
+    from random import choices, randint
+    import uharfbuzz as hb
+    font = DFont(font)
+
+    blob = hb.Blob.from_file_path(font.path)
+    hbface = hb.Face(blob)
+    hbfont = hb.Font(hbface)
+
+
+    glyphs_to_find = font.ttFont.getGlyphNames()
+    glyph_order = font.ttFont.getGlyphOrder()
+    ff = font.glyph_combinator.ff
+    ff.hoist_languages()
+    features = ff.features
+    scripts_and_langs = ff.scripts_and_languages
+    seen = {v: (chr(k), None, None, None) for k,v in font.ttFont.getBestCmap().items()}
+    strings = []
+    for script, langs in scripts_and_langs.items():
+        for lang in langs:
+            for feat, routines in features.items():
+                if feat == "aalt":
+                    continue
+                for routine in routines:
+                    routine = routine.routine
+                    glyph_sack = list(routine.involved_glyphs)
+
+                    char_input = [seen[c] for c in glyph_sack if c in seen]
+
+                    input_string = "".join([c[0] for c in char_input])
+                    input_langs = set(c[1] for c in char_input)
+                    input_scripts = set(c[2] for c in char_input)
+                    input_feats = {feat: True}
+                    input_feats[feat] = True
+
+                    buf = hb.Buffer()
+                    buf.add_str(input_string)
+                    buf.guess_segment_properties()
+
+                    hb.shape(hbfont, buf, features=input_feats)
+                    
+                    infos = buf.glyph_infos
+
+
+                    import pdb
+                    pdb.set_trace()
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python Lib/gftools/diffenator/fuzzer.py font1.ttf font2.ttf")
         sys.exit()
-    f1 = TTFont(sys.argv[1])
-    f2 = TTFont(sys.argv[2])
-    fuzz_fonts(f1, f2)
+    input_fuzz(sys.argv[1])
