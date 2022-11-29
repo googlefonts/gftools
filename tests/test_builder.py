f6433cc52cbd40f5d7c3dab87d42d64cda3f05c7
Simon Cozens <simon@simon-cozens.org>
2022-11-29 16:17:27 +0000

    Restore cwd to make test idempotent

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,4 +1,5 @@
 from gftools.builder import GFBuilder
+from contextlib import contextmanager
 import tempfile
 import pytest
 import yaml
@@ -6,6 +7,14 @@ import os
 from strictyaml import load, YAMLError
 from strictyaml.exceptions import YAMLValidationError
 
+
+@contextmanager
+def restore_cwd():
+    oldpwd = os.getcwd()
+    try:
+        yield
+    finally:
+        os.chdir(oldpwd)
 
 def monkey_patch_config_defaults(self):
     """we're not testing this"""
@@ -32,28 +41,31 @@ def config_file():
 
 def test_good_config(config_file):
     # check a good config file
-    with tempfile.NamedTemporaryFile("r+", suffix=".yml") as f:
-        f.write(yaml.dump(config_file))
-        f.seek(0)
-        GFBuilder(f.name)
+    with restore_cwd():
+        with tempfile.NamedTemporaryFile("r+", suffix=".yml") as f:
+            f.write(yaml.dump(config_file))
+            f.seek(0)
+            GFBuilder(f.name)
 
 
 def test_bad_config_structure(config_file):
     # change sources to a string when it should be a list
-    config_file["sources"] = "foobar"
-    with tempfile.NamedTemporaryFile("r+", suffix=".yml") as f:
-        f.write(yaml.dump(config_file))
-        f.seek(0)
-        with pytest.raises(ValueError, match="The yaml config file isn't structured properly"):
-            GFBuilder(f.name)
+    with restore_cwd():
+        config_file["sources"] = "foobar"
+        with tempfile.NamedTemporaryFile("r+", suffix=".yml") as f:
+            f.write(yaml.dump(config_file))
+            f.seek(0)
+            with pytest.raises(ValueError, match="The yaml config file isn't structured properly"):
+                GFBuilder(f.name)
 
 
 def test_bad_config_key(config_file):
     # Spell a key incorrectly
-    config_file["source"] = config_file["sources"]
-    del config_file["sources"]
-    with tempfile.NamedTemporaryFile("r+", suffix=".yml") as f:
-        f.write(yaml.dump(config_file))
-        f.seek(0)
-        with pytest.raises(YAMLError, match="A key in the configuration file"):
-            GFBuilder(f.name)
+    with restore_cwd():
+        config_file["source"] = config_file["sources"]
+        del config_file["sources"]
+        with tempfile.NamedTemporaryFile("r+", suffix=".yml") as f:
+            f.write(yaml.dump(config_file))
+            f.seek(0)
+            with pytest.raises(YAMLError, match="A key in the configuration file"):
+                GFBuilder(f.name)
