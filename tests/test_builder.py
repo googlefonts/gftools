97832e356013598d9bacce494bef3e5d0761fb44
Marc Foley <m.foley.88@gmail.com>
2023-03-24 10:58:42 +0000

    fix webfont move

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -5,16 +5,42 @@ import os
 import subprocess
 
 
+CWD = os.path.dirname(__file__)
+TEST_DIR = os.path.join(CWD, "..", "data", "test", "builder")
+
+
 @pytest.mark.parametrize(
     "fp,font_paths",
     [
+        # Tests our basic Glyphs setup. By default, otf, ttf, variable and
+        # webfonts are generated.
         (
-            os.path.join("data", "test", "builder", "check_compatibility_ufo_1"),
+            os.path.join(TEST_DIR, "basic_family_glyphs_0"),
+            [
+                os.path.join("variable", "TestFamily[wght].ttf"),
+                os.path.join("ttf", "TestFamily-Black.ttf"),
+                os.path.join("ttf", "TestFamily-Regular.ttf"),
+                os.path.join("ttf", "TestFamily-Thin.ttf"),
+                os.path.join("otf", "TestFamily-Black.otf"),
+                os.path.join("otf", "TestFamily-Regular.otf"),
+                os.path.join("otf", "TestFamily-Thin.otf"),
+                os.path.join("webfonts", "TestFamily[wght].woff2"),
+                os.path.join("webfonts", "TestFamily-Black.woff2"),
+                os.path.join("webfonts", "TestFamily-Regular.woff2"),
+                os.path.join("webfonts", "TestFamily-Thin.woff2"),
+            ]
+        ),
+        # Family consists of ufos which are not MM compatible. Tests
+        # https://github.com/googlefonts/gftools/pull/669
+        (
+            os.path.join(TEST_DIR, "check_compatibility_ufo_1"),
             [
                 os.path.join("ttf", "TestFamily-Black.ttf"),
                 os.path.join("ttf", "TestFamily-Thin.ttf"),
                 os.path.join("otf", "TestFamily-Black.otf"),
                 os.path.join("otf", "TestFamily-Thin.otf"),
+                os.path.join("webfonts", "TestFamily-Black.woff2"),
+                os.path.join("webfonts", "TestFamily-Thin.woff2"),
             ],
         ),
     ],
@@ -28,4 +54,4 @@ def test_builder(fp, font_paths):
         subprocess.run(["gftools", "builder", build_path])
         for font_path in font_paths:
             font_path = os.path.join(font_dir, font_path)
-            assert os.path.exists(font_path)
+            assert os.path.exists(font_path), f"{font_path} is missing"
