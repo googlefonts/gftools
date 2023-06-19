5048e5c219d181c80b3500f3d7ae67ed8fc94bf3
Marc Foley <m.foley.88@gmail.com>
2023-06-19 10:33:17 +0100

    run black

--- Lib/gftools/scripts/gen_push_lists.py
+++ Lib/gftools/scripts/gen_push_lists.py
@@ -29,8 +29,12 @@ def main(args=None):
 
     # get existing push items
     board_items = PushItems.from_traffic_jam()
-    sandbox_file = PushItems.from_server_file(to_sandbox_fp, PushStatus.IN_DEV, PushList.TO_SANDBOX)
-    production_file = PushItems.from_server_file(to_production_fp, PushStatus.IN_SANDBOX, PushList.TO_PRODUCTION)
+    sandbox_file = PushItems.from_server_file(
+        to_sandbox_fp, PushStatus.IN_DEV, PushList.TO_SANDBOX
+    )
+    production_file = PushItems.from_server_file(
+        to_production_fp, PushStatus.IN_SANDBOX, PushList.TO_PRODUCTION
+    )
 
     sandbox_board = board_items.to_sandbox()
     production_board = board_items.to_production()
