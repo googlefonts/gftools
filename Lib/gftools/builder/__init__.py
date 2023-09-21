6913a7cd6bb5df304146122cfb421de95e4e4a9b
Simon Cozens <simon@simon-cozens.org>
2023-09-21 13:22:21 +0100

    Move main function

--- Lib/gftools/builder/__init__.py
+++ Lib/gftools/builder/__init__.py
@@ -278,3 +278,24 @@ class GFBuilder:
             graphs[0].write_png("graph.png")
         else:
             print("Could not parse ninja build file")
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--graph", help="Draw a graph of the build process", action="store_true")
+    parser.add_argument("--no-ninja", help="Do not run ninja", action="store_true")
+    parser.add_argument("--generate", help="Just generate and output recipe from recipe builder", action="store_true")
+    parser.add_argument("config", help="Path to config file")
+    args = parser.parse_args()
+    pd = GFBuilder(args.config)
+    if args.generate:
+        print(yaml.dump(pd.config))
+        return
+    pd.config_to_objects()
+    pd.build_graph()
+    pd.walk_graph()
+    if args.graph:
+        pd.draw_graph()
+    if not args.no_ninja:
+        subprocess.run(["ninja"])
