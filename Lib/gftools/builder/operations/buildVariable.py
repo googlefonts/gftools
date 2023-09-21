25dee056328a7a4887cfc8213b2f9bab85d53322
Simon Cozens <simon@simon-cozens.org>
2023-09-21 11:36:13 +0100

    Add some validation for TTF builds

--- Lib/gftools/builder/operations/buildVariable.py
+++ Lib/gftools/builder/operations/buildVariable.py
@@ -4,13 +4,3 @@ from gftools.builder.operations import FontmakeOperationBase
 class BuildVariable(FontmakeOperationBase):
     description = "Build a variable font from a source file"
     rule = "fontmake --output-path $out -o variable $fontmake_type $in $fontmake_args"
-
-    @property
-    def variables(self):
-        vars = {}
-        if self.first_source.is_glyphs:
-            vars["fontmake_type"] = "-g"
-        else:
-            vars["fontmake_type"] = "-m"
-        
-        return vars
