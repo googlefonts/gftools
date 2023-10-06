6d8757266dc85571b475a5f7fed7fce172feb5d2
Simon Cozens <simon@simon-cozens.org>
2023-10-06 11:27:49 +0100

    Port more options from the old builder

--- Lib/gftools/builder/recipeproviders/googlefonts.py
+++ Lib/gftools/builder/recipeproviders/googlefonts.py
@@ -1,7 +1,20 @@
 import copy
 import os
+import logging
+from tempfile import NamedTemporaryFile
+
+import yaml
 from gftools.builder.recipeproviders import RecipeProviderBase
 
+logger = logging.getLogger("GFBuilder")
+
+# Things not ported from old builder:
+#  - cleanup
+#  - stylespace (old, replaced by stat)
+#  - axisOrder (did not actually do anything)
+#  - googleFonts (only used by actions, not builder)
+#  - interpolate (always on for statics now)
+#  - useMutatorMath (dead in fontmake)
 
 # Taken from gftools-builder
 DEFAULTS = {
@@ -15,7 +28,7 @@ DEFAULTS = {
     "buildTTF": True,
     "autohintTTF": True,
     "ttfaUseScript": False,
-    "logLevel": "INFO",
+    "logLevel": "WARN",
     "cleanUp": True,
     "includeSourceFixes": False,
     "fvarInstanceAxisDflts": None,
@@ -31,10 +44,21 @@ DEFAULTS = {
 
 class GFBuilder(RecipeProviderBase):
     def write_recipe(self):
+        if "instances" in self.config:
+            logger.warning(
+                "'instances' no longer supported; generate a config with --generate and select the instances you want"
+            )
+
         self.config = {**DEFAULTS, **self.config}
-        self.config["buildWebfont"] = (
-            self.config.get("buildWebfont", self.config.get("buildStatic", True))
+        self.config["buildWebfont"] = self.config.get(
+            "buildWebfont", self.config.get("buildStatic", True)
         )
+        if "stat" in self.config:
+            self.statfile = NamedTemporaryFile(delete=False)
+            yaml.dump(self.config["stat"], self.statfile)
+            self.statfile.close()
+        else:
+            self.statfile = None
         # Find variable fonts
         self.recipe = {}
         self.build_all_variables()
@@ -44,9 +68,32 @@ class GFBuilder(RecipeProviderBase):
     def fontmake_args(self):
         args = "--filter ... "
         if self.config.get("flattenComponents", True):
-            args += "--filter FlattenComponentsFilter "
+            args += " --filter FlattenComponentsFilter"
         if self.config.get("decomposeTransformedComponents", True):
-            args += "--filter DecomposeTransformedComponentsFilter "
+            args += " --filter DecomposeTransformedComponentsFilter"
+
+        if self.config.get("logLevel") != "WARN":
+            args += " --verbose " + self.config["logLevel"]
+        if self.config.get("reverseOutlineDirection"):
+            args += " --keep-direction"
+        if self.config.get("removeOutlineOverlaps") is False:
+            args += " --keep-overlaps"
+        if self.config.get("glyphData"):
+            args += " --glyph-data " + self.config["glyphData"]
+        if self.config.get("expandFeaturesToInstances"):
+            args += " --expand-features-to-instances"
+        return args
+
+    def fix_args(self):
+        args = ""
+        if self.config.get("includeSourceFixes"):
+            args += " --include-sources-fixes"
+        if self.config.get("fvarInstanceAxisDflts"):
+            args += (
+                " --fvar-instance-axis-dflts '"
+                + self.config["fvarInstanceAxisDflts"]
+                + "'"
+            )
         return args
 
     def build_all_variables(self):
@@ -67,31 +114,52 @@ class GFBuilder(RecipeProviderBase):
         all_variables = list(self.recipe.keys())
         if len(all_variables) > 0:
             last_target = all_variables[-1]
+            if self.statfile:
+                args = {"other_args": "--src " + self.statfile.name}
+            else:
+                args = {}
             self.recipe[last_target].append(
                 {
                     "postprocess": "buildStat",
                     "needs": list(set(all_variables) - set([last_target])),
+                    **args,
                 }
             )
 
     def build_a_variable(self, source):
         # Figure out target name
         sourcebase = os.path.splitext(source.basename)[0]
+        vf_args = ""
+
         if source.is_glyphs:
             tags = [ax.axisTag for ax in source.gsfont.axes]
+            if self.config.get("checkCompatibility") == False:
+                vf_args += "--no-check-compatibility "
         elif source.is_designspace:
             tags = [ax.tag for ax in source.designspace.axes]
         else:
             raise ValueError("Unknown source type")
+
         axis_tags = ",".join(sorted(tags))
 
         target = os.path.join(self.config["vfDir"], f"{sourcebase}[{axis_tags}].ttf")
         steps = [
             {"source": source.path},
-            {"operation": "buildVariable", "fontmake_args": self.fontmake_args()},
-            # XXX set version
-            {"operation": "fix"},
+            {
+                "operation": "buildVariable",
+                "fontmake_args": vf_args + self.fontmake_args(),
+            },
         ]
+        if os.path.basename(target) in self.config.get("vttSources", {}):
+            steps.append(
+                {
+                    "operation": "buildVTT",
+                    "vttfile": self.config["vttSources"][os.path.basename(target)],
+                }
+            )
+        steps.append(
+            {"operation": "fix", "fixargs": self.fix_args()},
+        )
         self.recipe[target] = steps
         if self.config["buildWebfont"]:
             target = os.path.join(
@@ -114,6 +182,9 @@ class GFBuilder(RecipeProviderBase):
             outdir = self.config["ttDir"]
         else:
             outdir = self.config["otDir"]
+        instancebase = os.path.splitext(os.path.basename(instance.filename))[0]
+        target = os.path.join(outdir, f"{instancebase}.{output}")
+
         steps = [
             {"source": source.path},
         ]
@@ -127,9 +198,20 @@ class GFBuilder(RecipeProviderBase):
                 "fontmake_args": self.fontmake_args(),
             }
         )
-        steps.append({"operation": "fix"})
-        instancebase = os.path.splitext(os.path.basename(instance.filename))[0]
-        target = os.path.join(outdir, f"{instancebase}.{output}")
+        if self.config.get("autohintTTF"):
+            if self.config.get("ttfaUseScript"):
+                args = "--auto-script"
+            else:
+                args = ""
+            steps.append({"operation": "autohint", "autohint_args": args})
+        if os.path.basename(target) in self.config.get("vttSources", {}):
+            steps.append(
+                {
+                    "operation": "buildVTT",
+                    "vttfile": self.config["vttSources"][os.path.basename(target)],
+                }
+            )
+        steps.append({"operation": "fix", "fixargs": self.fix_args()})
         self.recipe[target] = steps
 
         if self.config["buildWebfont"] and output == "ttf":
