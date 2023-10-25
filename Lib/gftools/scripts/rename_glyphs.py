4956dfcea719d5aae3d72561797b03832de86a33
Marc Foley <m.foley.88@gmail.com>
2023-10-25 10:44:06 +0100

    Implement simon feedback

--- Lib/gftools/scripts/rename_glyphs.py
+++ Lib/gftools/scripts/rename_glyphs.py
@@ -2,10 +2,10 @@
 Rename glyph names in font_1 with font_2.
 
 Usage:
-gftools rename-glyphs font_1.ttf font_2.ttf out font_1-renamed.ttf
+gftools rename-glyphs font_1.ttf font_2.ttf -o font_1-renamed.ttf
 """
 from fontTools.ttLib import TTFont
-from ufo2ft.postProcessor import PostProcessor, _reloadFont
+from ufo2ft.postProcessor import PostProcessor
 from argparse import ArgumentParser
 
 
@@ -18,10 +18,17 @@ def main(args=None):
 
     font1 = TTFont(args.font_1)
     font2 = TTFont(args.font_2)
-    
-    name_map = dict(zip(font1.getGlyphOrder(), font2.getGlyphOrder()))
 
-    font1 = _reloadFont(font1)
+    font1_glyphs = font1.getGlyphOrder()
+    font2_glyphs = font2.getGlyphOrder()
+
+    if len(font1_glyphs) != len(font2_glyphs):
+        raise ValueError(
+            "fonts must have same glyph counts. "
+            f"font1={len(font1_glyphs)}, font2={len(font2_glyphs)}"
+        )
+
+    name_map = dict(zip(font1_glyphs, font2_glyphs))
     PostProcessor.rename_glyphs(font1, name_map)
     font1.save(args.out)
 
