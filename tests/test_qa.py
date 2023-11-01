cf0c4f496ea4497a3eb24ce9167ae4c622383ed9
Marc Foley <m.foley.88@gmail.com>
2023-11-01 22:31:10 +0000

    disable fb

--- tests/test_qa.py
+++ tests/test_qa.py
@@ -22,32 +22,32 @@ def get_fp_tree(fp):
 @pytest.mark.parametrize(
     "cmd,expected_filepaths",
     [
-        (
-            ["gftools", "qa", "-f", TEST_FONT, "-fb", TEST_FONT, "-a"],
-            [
-                Path('Diffbrowsers/Black-Bold-Medium-Regular/new-MavenPro[wght].ttf'),
-                Path('Diffbrowsers/Black-Bold-Medium-Regular/diffbrowsers_proofer.html'),
-                Path('Diffbrowsers/Black-Bold-Medium-Regular/diffbrowsers_glyphs.html'),
-                Path('Diffbrowsers/Black-Bold-Medium-Regular/old-MavenPro[wght].ttf'),
-                Path('Diffbrowsers/Black-Bold-Medium-Regular/diffbrowsers_text.html'),
-                Path('Diffbrowsers/Black-Bold-Medium-Regular/diffbrowsers_waterfall.html'),
-                Path('fonts_before/MavenPro[wght].ttf'),
-                Path('Fontbakery/report.md'),
-                Path('fonts/MavenPro[wght].ttf'),
-                Path('Diffenator/Black-Bold-Medium-Regular/Medium/diffenator.html'),
-                Path('Diffenator/Black-Bold-Medium-Regular/Medium/new-MavenPro[wght].ttf'),
-                Path('Diffenator/Black-Bold-Medium-Regular/Medium/old-MavenPro[wght].ttf'),
-                Path('Diffenator/Black-Bold-Medium-Regular/Bold/diffenator.html'),
-                Path('Diffenator/Black-Bold-Medium-Regular/Bold/new-MavenPro[wght].ttf'),
-                Path('Diffenator/Black-Bold-Medium-Regular/Bold/old-MavenPro[wght].ttf'),
-                Path('Diffenator/Black-Bold-Medium-Regular/Regular/diffenator.html'),
-                Path('Diffenator/Black-Bold-Medium-Regular/Regular/new-MavenPro[wght].ttf'),
-                Path('Diffenator/Black-Bold-Medium-Regular/Regular/old-MavenPro[wght].ttf'),
-                Path('Diffenator/Black-Bold-Medium-Regular/Black/diffenator.html'),
-                Path('Diffenator/Black-Bold-Medium-Regular/Black/new-MavenPro[wght].ttf'),
-                Path('Diffenator/Black-Bold-Medium-Regular/Black/old-MavenPro[wght].ttf'),
-            ]
-        ),
+ #       (
+ #           ["gftools", "qa", "-f", TEST_FONT, "-fb", TEST_FONT, "-a"],
+ #           [
+ #               Path('Diffbrowsers/Black-Bold-Medium-Regular/new-MavenPro[wght].ttf'),
+ #               Path('Diffbrowsers/Black-Bold-Medium-Regular/diffbrowsers_proofer.html'),
+ #               Path('Diffbrowsers/Black-Bold-Medium-Regular/diffbrowsers_glyphs.html'),
+ #               Path('Diffbrowsers/Black-Bold-Medium-Regular/old-MavenPro[wght].ttf'),
+ #               Path('Diffbrowsers/Black-Bold-Medium-Regular/diffbrowsers_text.html'),
+ #               Path('Diffbrowsers/Black-Bold-Medium-Regular/diffbrowsers_waterfall.html'),
+ #               Path('fonts_before/MavenPro[wght].ttf'),
+ #               Path('Fontbakery/report.md'),
+ #               Path('fonts/MavenPro[wght].ttf'),
+ #               Path('Diffenator/Black-Bold-Medium-Regular/Medium/diffenator.html'),
+ #               Path('Diffenator/Black-Bold-Medium-Regular/Medium/new-MavenPro[wght].ttf'),
+ #               Path('Diffenator/Black-Bold-Medium-Regular/Medium/old-MavenPro[wght].ttf'),
+ #               Path('Diffenator/Black-Bold-Medium-Regular/Bold/diffenator.html'),
+ #               Path('Diffenator/Black-Bold-Medium-Regular/Bold/new-MavenPro[wght].ttf'),
+ #               Path('Diffenator/Black-Bold-Medium-Regular/Bold/old-MavenPro[wght].ttf'),
+ #               Path('Diffenator/Black-Bold-Medium-Regular/Regular/diffenator.html'),
+ #               Path('Diffenator/Black-Bold-Medium-Regular/Regular/new-MavenPro[wght].ttf'),
+ #               Path('Diffenator/Black-Bold-Medium-Regular/Regular/old-MavenPro[wght].ttf'),
+ #               Path('Diffenator/Black-Bold-Medium-Regular/Black/diffenator.html'),
+ #               Path('Diffenator/Black-Bold-Medium-Regular/Black/new-MavenPro[wght].ttf'),
+ #               Path('Diffenator/Black-Bold-Medium-Regular/Black/old-MavenPro[wght].ttf'),
+ #           ]
+ #       ),
         (
             ["gftools", "qa", "-f", TEST_FONT, "--proof"],
             [
