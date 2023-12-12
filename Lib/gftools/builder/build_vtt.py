4a16f26df34d2edfcf4fcf285cf043b9856fa833
Marc Foley <m.foley.88@gmail.com>
2023-12-12 16:10:34 +0000

    add missing import

--- Lib/gftools/builder/build_vtt.py
+++ Lib/gftools/builder/build_vtt.py
@@ -1,7 +1,7 @@
 import argparse
 from vttLib.transfer import merge_from_file as merge_vtt_hinting
 from vttLib import compile_instructions as compile_vtt_hinting
-from fontTools.ttLib import TTFont
+from fontTools.ttLib import TTFont, newTable
 
 
 def compile_vtt(font, vtt_source):
