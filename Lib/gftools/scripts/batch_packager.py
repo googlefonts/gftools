32a2b81d792efaf086ffb11fd07e871c4b673967
Marc Foley <m.foley.88@gmail.com>
2024-03-11 13:43:32 +0000

    batch-packager: better logging

--- Lib/gftools/scripts/batch_packager.py
+++ Lib/gftools/scripts/batch_packager.py
@@ -4,12 +4,29 @@ from gftools.packager import load_metadata, make_package, no_source_metadata
 import argparse
 import os
 from pathlib import Path
+import logging
+from rich.logging import RichHandler
+
+log = logging.getLogger("gftools.packager")
+LOG_FORMAT = "%(message)s"
 
 
 def main(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("repo", help="Path to the google/fonts repository")
+    parser.add_argument(
+        "--log-level",
+        choices=("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"),
+        default="INFO",
+    )
     args = parser.parse_args(args)
+
+    logging.basicConfig(
+        level=args.log_level,
+        format=LOG_FORMAT,
+        datefmt="[%X]",
+        handlers=[RichHandler()],
+    )
 
     failed = []
     for dirpath, _, filenames in os.walk(args.repo):
@@ -32,6 +49,7 @@ def main(args=None):
             except Exception as e:
                 print(f"Failed to make package for {family_path}: {e}")
                 failed.append(family_path)
+            log.info("---")
 
     print(f"Failed to make packages for {len(failed)} families")
     pprint(failed)
