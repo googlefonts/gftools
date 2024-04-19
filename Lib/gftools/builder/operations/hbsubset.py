4814dcbd3ac1705010392194ea52f563b148d23b
Simon Cozens <simon@simon-cozens.org>
2024-04-19 09:46:16 +0100

    Keep glyph names when subsetting (#922)

--- Lib/gftools/builder/operations/hbsubset.py
+++ Lib/gftools/builder/operations/hbsubset.py
@@ -3,4 +3,4 @@ from gftools.builder.operations import OperationBase
 
 class HbSubset(OperationBase):
     description = "Run hb-subset to slim down a font"
-    rule = "hb-subset --output-file=$in.subset --notdef-outline --unicodes=* --name-IDs=* --layout-features=* $in && mv $in.subset $out"
+    rule = "hb-subset --output-file=$in.subset --notdef-outline --unicodes=* --name-IDs=* --layout-features=* --glyph-names $args $in && mv $in.subset $out"
