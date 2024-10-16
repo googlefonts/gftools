b3723ae9e743c31ed9073230d437ed9f86b908ee
Colin Rofls <colin@cmyr.net>
2024-10-16 11:54:29 -0400

    [fontc] Skip instantiateUfo if running fontc

--- Lib/gftools/builder/recipeproviders/googlefonts.py
+++ Lib/gftools/builder/recipeproviders/googlefonts.py
@@ -323,7 +323,8 @@ class GFBuilder(RecipeProviderBase):
         steps = [
             {"source": source.path},
         ]
-        if not source.is_ufo:
+        # if we're running fontc we skip conversion to UFO
+        if not source.is_ufo and not self.config.get('use_fontc', False):
             instancename = instance.name
             if instancename is None:
                 if not instance.familyName or not instance.styleName:
