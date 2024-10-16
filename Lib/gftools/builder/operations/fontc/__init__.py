d28dada5a5f155e9b523a221f0ffac4be997674c
Colin Rofls <colin@cmyr.net>
2024-10-16 10:42:37 -0400

    [fontc] Rewrite fontmake args for fontc

---
+++ Lib/gftools/builder/operations/fontc/__init__.py
@@ -0,0 +1,44 @@
+from typing import List
+from gftools.builder.operations import OperationBase
+
+
+class FontcOperationBase(OperationBase):
+    @property
+    def variables(self):
+        vars = super().variables
+        args = vars.get("args")
+        if args:
+            vars["args"] = rewrite_fontmake_args_for_fontc(args)
+
+        return vars
+
+
+def rewrite_fontmake_args_for_fontc(args: str) -> str:
+    out_args = []
+    arg_list = args.split()
+    # reverse so we can pop in order
+    arg_list.reverse()
+    while arg_list:
+        out_args.append(rewrite_one_arg(arg_list))
+    return " ".join(out_args)
+
+
+# remove next arg from the front of the list and return its fontc equivalent
+def rewrite_one_arg(args: List[str]) -> str:
+    next_ = args.pop()
+    if next_ == "--filter":
+        filter_ = args.pop()
+        # this means 'retain filters defined in UFO', which... do we even support
+        # that in fontc?
+        if filter_ == "...":
+            pass
+        elif filter_ == "FlattenComponentsFilter":
+            return "--flatten-components"
+        elif filter_ == "DecomposeTransformedComponentsFilter":
+            return "--decompose-transformed-components"
+        else:
+            # glue the filter back together for better reporting below
+            next_ = f"{next_} {filter_}"
+    else:
+        raise ValueError(f"unknown fontmake arg '{next_}'")
+    return ""
--- Lib/gftools/builder/operations/fontc/fontcBuildTTF.py
+++ Lib/gftools/builder/operations/fontc/fontcBuildTTF.py
@@ -1,6 +1,6 @@
-from gftools.builder.operations import OperationBase
+from gftools.builder.operations.fontc import FontcOperationBase
 
 
-class FontcBuildTTF(OperationBase):
+class FontcBuildTTF(FontcOperationBase):
     description = "Build a TTF from a source file (with fontc)"
-    rule = "fontc -o $out $in"
+    rule = "fontc -o $out $in $args"
