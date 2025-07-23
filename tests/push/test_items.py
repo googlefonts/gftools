5f47022c0d2fdf6d8562cd125b48b7db04183d0d
Marc Foley <m.foley.88@gmail.com>
2025-07-23 10:41:23 +0100

    black

--- tests/push/test_items.py
+++ tests/push/test_items.py
@@ -17,7 +17,9 @@ TEST_DIR = os.path.join(CWD, "..", "..", "data", "test", "gf_fonts")
 SERVER_DIR = os.path.join(CWD, "..", "..", "data", "test", "servers")
 TEST_FAMILY_DIR = Path(TEST_DIR) / "ofl" / "mavenpro"
 DESIGNER_DIR = Path(TEST_DIR) / "joeprince"
-WEIGHT_AXIS = file_manager.enter_context(importlib.resources.path("axisregistry.data", "weight.textproto"))
+WEIGHT_AXIS = file_manager.enter_context(
+    importlib.resources.path("axisregistry.data", "weight.textproto")
+)
 FAMILY_JSON = json.load(open(os.path.join(SERVER_DIR, "family.json"), encoding="utf8"))
 FONTS_JSON = json.load(open(os.path.join(SERVER_DIR, "fonts.json"), encoding="utf8"))
 
