0296c4fa5afe1285b49c312dde687044c2ee2d83
Simon Cozens <simon@simon-cozens.org>
2026-02-09 12:28:48 +0000

    All right let's do this

---
+++ src/error.rs
@@ -0,0 +1,19 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum GftoolsError {
+    #[error("problem parsing font: {0}")]
+    FontParse(#[from] skrifa::raw::ReadError),
+    #[error("problem parsing JSON: {0}")]
+    JsonParse(#[from] serde_json_path_to_error::Error),
+    #[error("problem parsing TOML: {0}")]
+    TomlParse(#[from] toml::de::Error),
+    #[error("problem reading file: {0}")]
+    FileRead(#[from] std::io::Error),
+    #[error("miscellaneous error: {0}")]
+    Misc(String),
+    #[error("problem parsing protobuf: {0}")]
+    ProtobufParse(#[from] protobuf::text_format::ParseError),
+    #[error("HTTP error: {0}")]
+    HttpError(#[from] reqwest::Error),
+}
