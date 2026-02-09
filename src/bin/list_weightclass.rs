9e3c9c8ecaa3f982f26a0c996d6dfc466f84f525
Simon Cozens <simon@simon-cozens.org>
2026-02-09 15:14:14 +0000

    Port list_weightclass

--- src/bin/list_weightclass.rs
+++ src/bin/list_weightclass.rs
@@ -4,7 +4,7 @@ use gftools::list_some_things;
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
-/// Print out italicAngle of the fonts
+/// Print out usWeightClass of the fonts
 struct Args {
     fonts: Vec<String>,
     #[clap(long)]
@@ -13,12 +13,11 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-
     list_some_things(
         &args.fonts,
         |filename, fontref| {
-            if let Ok(post_table) = fontref.post() {
-                Some(vec![post_table.italic_angle().to_f32().to_string()])
+            if let Ok(os2_table) = fontref.os2() {
+                Some(vec![os2_table.us_weight_class().to_string()])
             } else {
                 log::warn!("{}: No 'post' table found, skipping", filename);
                 None
