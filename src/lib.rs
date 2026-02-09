15fd331cc8f9d12672144ea0665aa59df447c4fe
Simon Cozens <simon@simon-cozens.org>
2026-02-09 15:14:14 +0000

    Port list_weightclass

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use error::GftoolsError;
 #[allow(unused_imports)]
 pub(crate) use gf_metadata::DesignerInfoProto;
 pub(crate) use gf_metadata::{AxisProto, FamilyProto};
+use tabled::settings::Style;
 
 fn parse_metadatapb<T>(path: &Path) -> Result<T, GftoolsError>
 where
@@ -20,4 +21,45 @@ where
     let data = protobuf::text_format::parse_from_str::<T>(contents)
         .map_err(GftoolsError::ProtobufParse)?;
     Ok(data)
+}
+
+pub fn list_some_things<T: Into<String>>(
+    font_files: &[String],
+    lister: impl Fn(&str, &fontations::skrifa::FontRef) -> Option<Vec<T>>,
+    headers: &[&str],
+    csv: bool,
+) {
+    let mut info: Vec<Vec<String>> = Vec::new();
+    for font in font_files.iter() {
+        let Ok(font_data) = std::fs::read(font) else {
+            log::warn!("{}: Failed to read font file, skipping", font);
+            continue;
+        };
+        let Ok(fontref) = fontations::skrifa::FontRef::new(&font_data) else {
+            log::warn!("{}: Failed to parse font file, skipping", font);
+            continue;
+        };
+        if let Some(result) = lister(font, &fontref) {
+            info.push(
+                std::iter::once(font.to_string())
+                    .chain(result.into_iter().map(Into::into))
+                    .collect::<Vec<String>>(),
+            );
+        } // list should do its own error reporting
+    }
+    if csv {
+        println!("font,{}", headers.join(","));
+        for row in info {
+            println!("{}", row.join(","));
+        }
+    } else {
+        let mut builder = tabled::builder::Builder::default();
+        builder.push_record(headers.iter().map(|s| s.to_string()));
+        for row in info {
+            builder.push_record(row);
+        }
+        let mut table = builder.build();
+        table.with(Style::sharp());
+        println!("{}", table);
+    }
 }
