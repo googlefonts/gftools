1f5e1a667f785caa45295848c45e307075becc30
Ricky Atkins <ricky.atkins@daltonmaag.com>
2026-06-10 17:19:57 +0100

    Quote fontc path
    Allows for the use of path with whitespace

--- Lib/gftools/builder/operations/fontc/fontcBuildVariable.py
+++ Lib/gftools/builder/operations/fontc/fontcBuildVariable.py
@@ -3,4 +3,4 @@ from gftools.builder.operations.fontc import FontcOperationBase
 
 class FontcBuildVariable(FontcOperationBase):
     description = "Build a variable font from a source file (with fontc)"
-    rule = f"$fontc_path -o $out $in $args"
+    rule = "'$fontc_path' -o $out $in $args"
