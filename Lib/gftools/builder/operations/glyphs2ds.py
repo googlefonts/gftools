fb9d98f3e6893391986dbbd659b054ee24c58136
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2026-09-04 16:47:14 +0100

    glyphs2ds: use the base name of the source to know the output path

--- Lib/gftools/builder/operations/glyphs2ds.py
+++ Lib/gftools/builder/operations/glyphs2ds.py
@@ -1,14 +1,14 @@
 import os
+from pathlib import Path
 from tempfile import TemporaryDirectory
 
 from gftools.builder.file import File
 from gftools.builder.operations import OperationBase
-from glyphsLib.builder.axes import find_base_style
 
 
 class Glyphs2DS(OperationBase):
     description = "Turn a Glyphs file into a Designspace file"
-    rule = "fontmake -o ufo -g $in --output-dir $outdir $fontmake_args"
+    rule = "fontmake -o ufo -g $in --output-dir $outdir --designspace-path $dspath $fontmake_args"
 
     def convert_dependencies(self, builder):
         self._target = TemporaryDirectory()  # Stow object
@@ -19,18 +19,13 @@ class Glyphs2DS(OperationBase):
         if "directory" in self.original:
             target = self.original["directory"]
 
-        base_family = self.first_source.family_name
-        # glyphsLib has a special case for Glyphs files where the masters have a
-        # common style name; typically this manifests itself in single-master
-        # Glyphs files, but it can also happen in multi-master files.
-        # In that case, the designspace filename will be changed.
-        if base_style := find_base_style(self.first_source.gsfont.masters):
-            base_family += "-" + base_style
-        dspath = os.path.join(target, base_family.replace(" ", "") + ".designspace")
+        base_name = Path(self.first_source.path).with_suffix(".designspace").name
+        dspath = os.path.join(target, base_name)
         return [File(dspath)]
 
     @property
     def variables(self):
         return {
             "outdir": os.path.dirname(self.targets[0].path),
+            "dspath": self.targets[0].path,
         }
